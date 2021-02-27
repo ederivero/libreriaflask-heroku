5e9a08a4e1eb0c4000fe3f8f37c15144d82aef1e
Eduardo de Rivero <eduardo.derivero@valigo.org>
2021-02-26 21:15:44 -0500

    cambie ruta de base de datos

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ app.register_blueprint(swagger_blueprint)
 
 # https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/#connection-uri-format
 #                                    formato://username:password@host:port/databasename
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/flasklibreria'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://numjmbfsfzvdbzfp:f4xhk9fzmaayj85s@d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/t7pdjw13yz44k2hq'
 api = Api(app)
 CORS(app) # PERMITIENDO TODOS LOS METODOS , DOMINIOS Y HEADERS
 
